057cddb6737f87b20b5eef8277a5e478024b0ea6
Jerome Lefeuvre <jerome.lefeuvre@gmail.com>
2012-12-18 13:10:55 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultiSports::Application.config.secret_token = 'ddfb0a738df04c28d704d08dd0821749d4e36d715d3ee43902ed2d5c5f9533af23c5c71eba6b152790b36bbba43170cf863b2ce1325d32b30561dd9f041771f9'
